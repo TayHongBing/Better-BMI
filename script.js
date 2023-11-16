d02a51bdb0459ce45f255065f5c3ac90656c4fb7
TayHongBing <93114029+TayHongBing@users.noreply.github.com>
2023-11-16 13:31:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculateBMI() {
+  const firstNameInput = document.getElementById("firstName");
+  const heightInput = document.getElementById("height");
+  const weightInput = document.getElementById("weight");
+  const result = document.getElementById("result");
+
+  const firstName = firstNameInput.value;
+  const height = heightInput.value;
+  const weight = weightInput.value;
+  
+  bmi = weight / height ** 2
+  console.log(firstName)
+  console.log(weight)
+  console.log(height)
+
+  result.innerHTML = `${firstName}, your BMI is ${bmi.toFixed(2)}!`;
+}
+
+
